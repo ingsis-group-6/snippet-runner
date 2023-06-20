44004a3f3f126b163a9d88c7c83c8b2fb4fead99
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-06-20 19:32:03 -0300

    Implementing run function

--- src/main/kotlin/ingsis/snippetrunner/service/PrintscriptRunnerService.kt
+++ src/main/kotlin/ingsis/snippetrunner/service/PrintscriptRunnerService.kt
@@ -1,6 +1,8 @@
 package ingsis.snippetrunner.service
 
+import ingsis.snippetrunner.error.HTTPError
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpClientErrorException
 import java.util.*
@@ -8,12 +10,21 @@ import java.util.*
 @Service
 class PrintscriptRunnerService(@Autowired private val httpService: HttpService): RunnerService {
     override fun run(token: String, snippetId: UUID, languageVersion: String) {
+
         try{
             val snippetManagerResponse = this.httpService.getSnippetCodeFromManager(token, snippetId)
             print(snippetManagerResponse.content)
-        } catch (exc: HttpClientErrorException) {
-            println("Exception: ${exc.message}")
+        } catch (ex: HttpClientErrorException.NotFound) {
+            throw HTTPError("Snippet not found", HttpStatus.NOT_FOUND)
+        } catch (ex: HttpClientErrorException.Forbidden) {
+            throw HTTPError("You are not allowed to access this snippet", HttpStatus.FORBIDDEN)
+        } catch (ex: HttpClientErrorException.Unauthorized) {
+            throw HTTPError("You are not authorized to access this snippet", HttpStatus.UNAUTHORIZED)
+        } catch (ex: HttpClientErrorException) {
+            throw HTTPError("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR)
         }
+
+
 
 
     }
