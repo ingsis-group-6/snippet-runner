d8db733593be8377a3248454e73adf2eb8fa8f1e
ricardolopezb <ricardolopezbonaguro@gmail.com>
2023-07-03 19:55:19 -0300

    downgraded

--- src/main/kotlin/ingsis/snippetrunner/SnippetRunnerApplication.kt
+++ src/main/kotlin/ingsis/snippetrunner/SnippetRunnerApplication.kt
@@ -2,8 +2,10 @@ package ingsis.snippetrunner
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.metrics.export.datadog.EnableDatadogMetrics
 
 @SpringBootApplication
+@EnableDatadogMetrics
 class SnippetRunnerApplication
 
 fun main(args: Array<String>) {
